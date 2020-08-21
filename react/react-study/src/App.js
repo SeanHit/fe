d282968bc99a26d88b44981f68722b55563e70d9
liuxiang.myall <liuxiang.myall@bytedance.com>
2020-08-21 09:56:45 +0800

    暂存提交

--- react/react-study/src/App.js
+++ react/react-study/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Clock from './pages/lifeCycle/Clock'
+import Event from './pages/Event'
+import BoilingVerdict from './pages/statePromotion/BoilingVerdict.jsx'
 
 function App() {
   return (
     <div className="App">
-      <Clock/>
+      <BoilingVerdict />
     </div>
   );
 }
---
+++ react/react-study/src/pages/statePromotion/BoilingVerdict.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default  function BoilingVerdict(props){
+  return (<div>lslsl</div>)
+}
